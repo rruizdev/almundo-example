7645d398c03c21206e5e09e5105c104e7c3f29f6
Roberto Ruiz <rruiz@est.frba.utn.edu.ar>
2019-09-26 02:20:32 -0300

    Create a nodejs backend with SwaggerUI

---
+++ backend/app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const router = require('express').Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+const expressSwagger = require('express-swagger-generator')(app);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+router.use('/api-docs', swaggerUi.serve);
+
+var options = {
+    swaggerOptions: {
+        validatorUrl: null
+    }
+};
+
+router.get('/api-docs', swaggerUi.setup(swaggerDocument, options));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+expressSwagger({
+    swaggerDefinition: {
+        info: {
+            description: 'Backend for demo purposes',
+            title: 'almundo.backend',
+            version: '1.0.0',
+        },
+        host: 'localhost:3000',
+        basePath: '/v1',
+        produces: [
+            "application/json",
+            "application/xml"
+        ],
+        schemes: ['http', 'https']
+    },
+    basedir: __dirname,
+    files: ['./routes/**/*.js']
+});
+
+module.exports = app;
