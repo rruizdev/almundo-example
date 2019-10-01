0aac932341f12553b90ab0db2280254aec6f89c9
Roberto Ruiz <rruiz@est.frba.utn.edu.ar>
2019-10-01 13:37:59 -0300

    Add service

--- frontend/src/app/services/hotels.service.ts
+++ frontend/src/app/services/hotels.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HotelsService {
+  private hotels = new BehaviorSubject<any[]>([]);
+  public hotels$ = this.hotels.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  get() {
+    this.hotels.next([]);
+    this.http.get('http://localhost:3000/hotels')
+      .subscribe((response: any) => this.hotels.next(response));
+  }
 }
