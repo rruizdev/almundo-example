c7d8552d5bfc1536a020e0d6a98164b5fff962d2
Roberto Ruiz <rruiz@est.frba.utn.edu.ar>
2019-10-01 14:00:50 -0300

    Add backend invocation

--- frontend/src/app/components/detail/detail.component.ts
+++ frontend/src/app/components/detail/detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HotelsService } from 'src/app/services/hotels.service';
 
 @Component({
   selector: 'app-detail',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.component.sass']
 })
 export class DetailComponent implements OnInit {
+  hotels = []
 
-  constructor() { }
+  constructor(private service: HotelsService) { }
 
   ngOnInit() {
+    this.service.hotels$.subscribe(response => this.hotels = response);
+    this.service.get();
   }
-
 }
